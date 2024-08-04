0884f2b6a17567e9b8f7b0e077e67007412c5008
payce06 <paycegrossman@gmail.com>
2024-08-04 10:45:30 -0400

    assignment-2

---
+++ assignment2.js
@@ -0,0 +1,60 @@
+// Operators
+// Write a function that takes two numbers and returns their sum.
+function addNumbers(a, b){
+    const add =  a + b
+    return add
+    }
+
+const num1 = 5
+const num2 = 10
+const result1 = addNumbers(num1, num2)
+console.log(`The numbers added: ${result1}`)
+console.log(`=========================================`)
+
+// Write a function that takes two numbers and returns their difference.
+function subNumbers(a, b){
+    const sub = a - b
+    return sub
+}
+
+const num3 = 30
+const num4 = 10
+const result2 = subNumbers(num3, num4)
+console.log(`The numbers subtracter: ${result2}`)
+console.log(`=============================================`)
+
+// Write a function that takes two numbers and returns their product.
+function multiplyNumbers(a, b){
+    const multiply = a * b
+    return multiply
+}
+
+const num5 = 6
+const num6 = 10
+const result3 = multiplyNumbers(num5, num6)
+console.log(`The numbers multiplied: ${result3}`)
+console.log(`======================================`)
+
+// Write a function that takes two numbers and returns their quotient.
+function divideNumbers(a, b){
+    const divide = a / b
+    return divide
+}
+
+const num7 = 10
+const num8 = 10
+const result4 = divideNumbers(num7, num8)
+console.log(`The numbers divided: ${result4}`)
+console.log(`===========================================`)
+
+// Write a function that takes two numbers and returns the remainder when the first is divided by the second.
+function takeRemainder(a, b){
+    const remainder = a % b
+    return remainder
+}
+
+const num9 = 10
+const num10 = 3
+const result5 = takeRemainder(num9, num10)
+console.log(`The remainder of the numbers: ${result5}`)
+console.log(`============================================`)
