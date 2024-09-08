b93d2ab3892ee8dd2e69cb615ca308a30a7bec8e
payce06 <paycegrossman@gmail.com>
2024-09-07 22:02:15 -0400

    recursion-explanation

--- recursions.js
+++ recursions.js
@@ -46,3 +46,78 @@ function PrintNumbers(num) {
 PrintNumbers(5)
 
 console.log(`========================================================`)
+
+function printNumbers(arr, i){
+    if(i < 0){
+        return
+    }
+    console.log(arr[i])
+    printNumbers(arr, i -1)
+}
+const array1 = [1, 7, 2, 6, 2, 6]
+printNumbers(array1, array1.length -1)
+
+console.log(`========================================================`)
+
+function printLetters(string, i){
+    if(i > string.length -1){
+        return
+    }
+    console.log(string[i])
+    printLetters(string, i + 1)
+}
+const string1 = "Payce"
+printLetters(string1, 0)
+
+console.log(`===========================================================`)
+
+function printLetters(string, i){
+    if(i < 0){
+        return
+    }
+    console.log(string[i])
+    printLetters(string, i - 1)
+}
+const string1 = "Payce"
+printLetters(string1, string1.length -1)
+
+console.log(`============================================================`)
+
+function calculateSum(num) {
+    console.log(`Called function with ${num}`)
+    if (num < 1) {
+        console.log("Based Case Entered. Sum = 0")
+        return 0
+    }
+   
+   
+    // For this function to do its part.
+    // For total sum: I am adding the current number and the sum of
+    // numbers returned by its children
+    let totalSum = num + calculateSum(num - 1)
+   
+   
+    console.log(`Returning for function with ${num}. Sum = ${totalSum}`)
+    return totalSum
+}
+
+
+// Calculates sum of the numbers from 1 to num
+calculateSum(5)
+
+
+
+// calculateSum(5)
+// 5 + calculateSum(4)
+// 5 + (4 + calculateSum(3))
+// 5 + (4 + (3 + calculateSum(2)))
+// 5 + (4 + (3 + (2 + calculateSum(1))))
+// 5 + (4 + (3 + (2 + (1 + calculateSum(0))))
+// 5 + (4 + (3 + (2 + (1 + 0)))
+// 5 + (4 + (3 + (2 + 1))
+// 5 + (4 + (3 + 3))
+// 5 + (4 + 6)
+// 5 + 10
+// 15
+
+console.log(`===================================================================`)
