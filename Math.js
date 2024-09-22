79dea46a0f66565b7be52d3440b851ef35a874e8
payce06 <paycegrossman@gmail.com>
2024-09-21 21:54:16 -0400

    math -4

--- Math.js
+++ Math.js
@@ -82,3 +82,146 @@ function totalPay(hours, payPerHour) {
 
 console.log(`================================================================================`)
 
+// Write a function that takes in an integer, n and returns the sum of all integers from 0 to n.
+
+function addUpTo(n) {
+    let sum = 0
+    for(let i = 0; i <= n; i++){
+        sum = sum + i
+    }
+    return sum
+  }
+
+  console.log(`============================================================================`)
+
+//   Write a function that takes in two numbers, an operator (+, -, *, /) and the result of the calculation. The function should check if the calculation is correct and return either true or false.
+
+function calculator(num1, operator, num2, sol) {
+    let checkSol = 0
+	if(operator == "+"){
+	    checkSol = num1 + num2
+	} else if(operator == "-"){
+	    checkSol = num1 - num2
+	} else if(operator == "*"){
+	    checkSol = num1 * num2
+	} else {
+	    checkSol = num1 / num2
+	}
+	if(checkSol == sol){
+	    return true
+	} else {
+	    return false
+	}
+}
+
+console.log(`===============================================================================`)
+
+// Write a function that takes in three numbers that represent he coefficients of the quadratic equation. Using the quadratic formula, return the solutions of the equation.
+
+// Name your solutions sol1 and sol2 and use the return statement below.
+
+// return "Solutions: " + sol1 + " and " + sol2;
+
+function quadraticEquation(a, b, c) {
+    let sol1 = (-b + Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+    let sol2 = (-b - Math.sqrt(b * b - 4 * a * c)) / (2 * a)
+    return "Solutions: " + sol1 + " and " + sol2
+}
+
+console.log(`========================================================================`)
+
+// Write the function equal that compares the first element arrays, a and b, and returns true if they’re equal.
+
+// You can assume each array has at least one element.
+
+function compareFirst(a, b) {
+    if(a[0] == b[0]){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(`===========================================================================`)
+
+// Write a function that returns true if the first and last element of a list are the same, and false otherwise.
+
+// You can assume the list has at least one element.
+
+function beginningAndEnd(array) {
+    if(array[0] == array[array.length -1]){
+        return true
+    } else {
+        return false
+    }
+}
+
+console.log(`=======================================================================`)
+
+// Write the doubleEverything function which takes an array of numbers and returns an array where all of those numbers are doubled.
+
+// For example,
+
+doubleEverything([1,2,3]) => [2, 4, 6]
+
+function doubleEverything(array) {
+    for(let i = 0; i <= array.length -1; i++){
+        array[i] = array[i] * 2
+    }
+    return array
+}
+
+console.log(`==========================================================`)
+
+// Write the function negative which takes an array of numbers and returns an array where all of those numbers are negative.
+
+// For example,
+
+negative([-10, 20, 30]) => [-10, -20, -3]
+
+console.log(`========================================================`)
+
+// Write the sum function which returns the sum of all numbers in an array.
+
+// You can assume there are only numbers in the array, but not that the array isn’t empty! If the array is empty, you should return 0.
+
+// For example:
+// sum([1, 2, 3]) => 6
+// sum([]) => 0
+
+
+function sum(array) {
+    let result = 0
+    for(let i = 0; i <= array.length -1; i++){
+        result = result + array[i]
+    }
+    return result
+}
+
+console.log(`=======================================================`)
+
+// Write the repeats function which returns true if an element appears more than once in an array and false otherwise.
+
+// For example:
+// repeats([]) => false
+// repeats([1,2,3]) => false
+// repeats([1,2,1]) => true
+// repeats(['a', 'b', 'c']) => true
+
+function repeats(array) {
+    let object= {}
+    for(let i = 0; i <= array.length -1; i++){
+        const current = array[i]
+        if(!object[current]){
+            object[current] = 1
+        } else {
+            return true
+        }
+    } 
+    return false
+}
+
+console.log(`=======================================================================`)
+
+
+
