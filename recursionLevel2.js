8b7447945b9003d4d767f71278205236af8cd9f1
payce06 <paycegrossman@gmail.com>
2024-09-22 21:04:29 -0400

    recursion-2

--- recursionLevel2.js
+++ recursionLevel2.js
@@ -42,3 +42,72 @@ const arr2 = [1, 4, 6, 8, 10, 12, 16, 18, 20]
 const result = mergeArrays(arr1, arr2)
 console.log(`Result: ${result}`)
 
+console.log(`========================================================================`)
+
+function mergeArrays(arr1, arr2) {
+    let result = []
+    let leftIndex = 0
+    let rightIndex = 0
+   
+   
+    while (leftIndex < arr1.length && rightIndex < arr2.length) {
+       
+       
+        if (arr1[leftIndex] < arr2[rightIndex]) {
+            result.push(arr1[leftIndex])
+            leftIndex += 1
+        } else {
+            result.push(arr2[rightIndex])
+            rightIndex += 1
+        }
+    }
+   
+    while (leftIndex < arr1.length) {
+        result.push(arr1[leftIndex])
+        leftIndex += 1
+    }
+   
+  
+    while (rightIndex < arr2.length) {
+        result.push(arr2[rightIndex])
+        rightIndex += 1
+    }
+   
+    return result
+}
+
+
+
+function mergeSort(arr){
+    if(arr.length <= 1){
+        return arr
+    }
+    const mid = Math.floor(arr.length / 2)
+    
+    const leftArr = arr.slice(0, mid)
+    const rightArr = arr.slice(mid, arr.length)
+    
+    const leftSortedArr = mergeSort(leftArr)
+    const rightSortedArr = mergeSort(rightArr)
+    
+    const result = mergeArrays(leftSortedArr, rightSortedArr)
+    
+    return result
+    
+}
+
+console.log(mergeSort([1, 5, 2, 7, 4]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
